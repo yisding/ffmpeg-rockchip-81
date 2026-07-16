nyanmisaka/ffmpeg-rockchip

lavf/rkrga: add RKRGA scale, vpp and overlay filter
Signed-off-by: nyanmisaka <nst799610810@gmail.com>

libavfilter/allfilters.c
@@ -387,6 +387,7 @@ extern const AVFilter ff_vf_overlay_qsv;
 extern const AVFilter ff_vf_overlay_vaapi;
 extern const AVFilter ff_vf_overlay_vulkan;
 extern const AVFilter ff_vf_overlay_cuda;
+extern const AVFilter ff_vf_overlay_rkrga;
 extern const AVFilter ff_vf_owdenoise;
 extern const AVFilter ff_vf_pad;
 extern const AVFilter ff_vf_pad_opencl;
@@ -432,6 +433,7 @@ extern const AVFilter ff_vf_scale_qsv;
 extern const AVFilter ff_vf_scale_vaapi;
 extern const AVFilter ff_vf_scale_vt;
 extern const AVFilter ff_vf_scale_vulkan;
+extern const AVFilter ff_vf_scale_rkrga;
 extern const AVFilter ff_vf_scale2ref;
 extern const AVFilter ff_vf_scale2ref_npp;
 extern const AVFilter ff_vf_scdet;
@@ -517,6 +519,7 @@ extern const AVFilter ff_vf_vif;
 extern const AVFilter ff_vf_vignette;
 extern const AVFilter ff_vf_vmafmotion;
 extern const AVFilter ff_vf_vpp_qsv;
+extern const AVFilter ff_vf_vpp_rkrga;
 extern const AVFilter ff_vf_vstack;
 extern const AVFilter ff_vf_w3fdif;
 extern const AVFilter ff_vf_waveform;

yisding/ffmpeg-rockchip-81

libavfilter/allfilters.c
@@ -399,6 +399,7 @@ extern const FFFilter ff_vf_overlay_qsv;
 extern const FFFilter ff_vf_overlay_vaapi;
 extern const FFFilter ff_vf_overlay_vulkan;
 extern const FFFilter ff_vf_overlay_cuda;
+extern const FFFilter ff_vf_overlay_rkrga;
 extern const FFFilter ff_vf_owdenoise;
 extern const FFFilter ff_vf_pad;
 extern const FFFilter ff_vf_pad_cuda;
@@ -451,6 +452,7 @@ extern const FFFilter ff_vf_scale_qsv;
 extern const FFFilter ff_vf_scale_vaapi;
 extern const FFFilter ff_vf_scale_vt;
 extern const FFFilter ff_vf_scale_vulkan;
+extern const FFFilter ff_vf_scale_rkrga;
 extern const FFFilter ff_vf_scale2ref;
 extern const FFFilter ff_vf_scale2ref_npp;
 extern const FFFilter ff_vf_scdet;
@@ -538,6 +540,7 @@ extern const FFFilter ff_vf_vif;
 extern const FFFilter ff_vf_vignette;
 extern const FFFilter ff_vf_vmafmotion;
 extern const FFFilter ff_vf_vpp_qsv;
+extern const FFFilter ff_vf_vpp_rkrga;
 extern const FFFilter ff_vf_vstack;
 extern const FFFilter ff_vf_w3fdif;
 extern const FFFilter ff_vf_waveform;